6f8ae31f10252f35bdf657f5efb218b5b5a7beae
kate <kata3140@gmail.com>
2024-03-22 21:39:29 +0200

    new

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,8 +1,9 @@
-const setButton = (buttonText) => {
+export const setButton = (buttonText) => {
     let parent = document.querySelector('body');
     const button = document.createElement('button');
     button.innerHTML = buttonText;
     parent.append(button);
 }
+
 
 setButton('button text');
