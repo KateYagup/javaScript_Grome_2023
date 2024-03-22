e5afdab5eaa18535e8e4116430d8c01dada9966b
kate <kata3140@gmail.com>
2024-03-22 11:17:43 +0200

    lesson21

---
+++ lesson21/task1/index.js
@@ -0,0 +1,16 @@
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.log(titleElem);
+    return titleElem;
+}
+
+export const getInputElement = () => {
+    // const inputElem = document.getElementsByTagName('input')[0].value;
+    // const inputElem = document.getElementsByTagName('input')[0]
+    const inputElem = document.querySelector('input');
+    console.log(inputElem);
+    return inputElem;
+}
+
+getTitleElement();
+getInputElement();
