b4198fe488f4e2e5f172fe0008b54b9e08213f47
kate <kata3140@gmail.com>
2024-03-22 12:47:55 +0200

    task3

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,6 +1,29 @@
-const getTitle = () => {
-    const elTitle = document.querySelector(.title);
-    console.log(elTitle.textContent);
+export const getTitle = () => {
+    const elTitle = document.querySelector('.title');
+    // console.log(elTitle.textContent);
+    return elTitle.textContent;
 }
 
-getTitle();
+export const getDescription = () => {
+    const elDescr = document.querySelector('.about');
+    // console.log(elDescr.innerText);
+    return elDescr.innerText;
+}
+
+export const getPlans = () => {
+    const elPlans = document.querySelector('.plans');
+    // console.log(elPlans.innerHTML);
+    return elPlans.innerHTML;
+}
+
+export const getGoal = () => {
+    const elGoal = document.querySelector('.goal');
+    // console.log(elGoal.outerHTML);
+    // console.log(elGoal);
+    return elGoal.outerHTML;
+}
+
+// getTitle();
+// getDescription();
+// getPlans();
+// getGoal();
