f96e3e015a3a1de70fedb865ed987e88cc99ce5d
kate <kata3140@gmail.com>
2024-03-24 21:05:59 +0200

    task11

---
+++ lesson21/task11/index.js
@@ -0,0 +1,8 @@
+export const squaredNumbers = () => {
+    const items = document.querySelectorAll('li');
+    for (let item of items) {
+        item.dataset.squaredNumber = item.dataset.number ** 2;
+    }
+}
+
+squaredNumbers();
