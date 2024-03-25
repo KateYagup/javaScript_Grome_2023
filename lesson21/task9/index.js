25a67da454f6d26459af73928c5bf8e25d9f5df3
kate <kata3140@gmail.com>
2024-03-25 11:24:00 +0200

    new

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -3,6 +3,7 @@ export const finishForm = () => {
     const firstInput = document.querySelector('input');
     const login = document.createElement('input');
     login.setAttribute('name', 'login');
+    login.setAttribute('type', 'text');
     firstInput.setAttribute('type', 'password');
     form.prepend(login);
 }
