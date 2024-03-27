3bfb3739a76966dfdd148990ec5665a4b81b66ac
kate <kata3140@gmail.com>
2024-03-27 10:25:43 +0200

    new

--- lesson22/task3/index.js
+++ lesson22/task3/index.js
@@ -1,6 +1,5 @@
 const checkboxElem = document.querySelector('.task-status');
 checkboxElem.addEventListener('click', () => {
-    if (checkboxElem.checked) console.log('true')
-    else console.log('false');
+    console.log(checkboxElem.checked);
     console.log(checkboxElem.value);
 })
