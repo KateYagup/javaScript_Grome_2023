382048dfd79ba2e54ac20b12ac280cccbd9864b7
kate <kata3140@gmail.com>
2024-04-06 11:58:34 +0300

    new

--- lesson24/example1/index.js
+++ lesson24/example1/index.js
@@ -1,12 +1,15 @@
-const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-const getDayOfWeek = (date, days) => {
+
+
+export const getDayOfWeek = (date, days) => {
+    const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
     // console.log('dateInFuture' + dateInFuture);
     // console.log(new Date(dateInFuture).getDay());
     return weekDays[new Date(dateInFuture).getDay()];
 }
+
 
 const result = getDayOfWeek(new Date(2024, 4, 5), 14);
 console.log(result);
