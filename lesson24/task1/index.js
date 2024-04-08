0a2020a8a9c88c7731d1eb2eb7dc561798557d30
kate <kata3140@gmail.com>
2024-04-08 21:41:52 +0300

    new

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,7 +1,7 @@
 export const getDiff = (startDate, endDate) => {
     const diffDays = endDate.getDay() - startDate.getDay();
 
-    const difference = endDate - startDate;
+    const difference = Math.abs(endDate - startDate);
     const secAll = difference / 1000;
     const minutesAll = difference / (1000 * 60);
     const hoursAll = difference / (1000 * 60 * 60);
@@ -11,8 +11,14 @@ export const getDiff = (startDate, endDate) => {
     const minutesRemain = Math.floor(minutesAll - Math.floor(hoursAll) * 60);
     const secondsRemain = Math.floor(secAll - Math.floor(minutesAll) * 60);
 
+    console.log(difference);
+    console.log(`${daysAll}d ${hourRemain}h ${minutesRemain}m ${secondsRemain}s`);
     return `${daysAll}d ${hourRemain}h ${minutesRemain}m ${secondsRemain}s`;
 }
+
+// const startDate = new Date(Date.UTC(2019, 10, 24, 20, 50, 34, 44));
+// const endDate = new Date(Date.UTC(2019, 1, 22, 17, 52, 18));
+// getDiff(startDate, endDate);
 
 // const originalDate = new Date(2023, 2, 3, 12);
 // const currentDate = new Date();
