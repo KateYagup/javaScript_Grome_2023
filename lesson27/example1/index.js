ec18a1a6a2414e08c7f6a286747510370e1402a9
kate <kata3140@gmail.com>
2024-04-12 13:31:23 +0300

    new

--- lesson27/example1/index.js
+++ lesson27/example1/index.js
@@ -8,20 +8,22 @@ localStorage.setItem('age', JSON.stringify(17));
 // console.log(JSON.parse(localStorage.getItem('hobbies')));
 
 export const getLocalStorageData = () => {
-    return Object.entries(localStorage)
-        .reduce((acc, [key, value]) => {
-            let newValue;
-            try {
-                newValue = JSON.parse(value);
-            } catch (e) {
-                newValue = value;
-            }
+    return {
+        Object.entries(localStorage)
+            .reduce((acc, [key, value]) => {
+                let newValue;
+                try {
+                    newValue = JSON.parse(value);
+                } catch (e) {
+                    newValue = value;
+                }
 
-            return {
-                ...acc, // возвращаем значения, которые уже есть в аккумуляторе
-                [key]: JSON.parse(value), // добавляем новый ключ
-            };
-        }, {});
+                return {
+                    ...acc, // возвращаем значения, которые уже есть в аккумуляторе
+                    [key]: JSON.parse(value), // добавляем новый ключ
+                };
+            }, {})
+    }
 }
 
 // console.log(getLocalStorageData());
