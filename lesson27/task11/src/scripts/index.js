192436f4372bc7781b4722821a0f11055ecab6e5
kate <kata3140@gmail.com>
2024-04-25 19:12:17 +0300

    new

--- lesson27/task11/src/scripts/index.js
+++ lesson27/task11/src/scripts/index.js
@@ -1,11 +1,11 @@
-import { tasks } from './storage.js';
+// import { tasks } from './storage.js';
 import { renderTasks } from './renderer.js';
 import { onToggleTask } from './updateTask.js';
 import { onCreateTask } from './createTask.js';
 
 const listElem = document.querySelector('.list');
 
-renderTasks(tasks);
+renderTasks();
 
 listElem.addEventListener('click', onToggleTask);
 
@@ -13,3 +13,10 @@ const createBtnElem = document.querySelector('.create-task-btn');
 createBtnElem.addEventListener('click', onCreateTask);
 
 
+const onStorageCase = e => {
+    if (e.key === 'tasksList') {
+        renderTasks();
+    }
+}
+
+window.addEventListener('storage', onStorageCase);
--- lesson27/task11/src/scripts/storage.js
+++ lesson27/task11/src/scripts/storage.js
@@ -6,6 +6,17 @@
 //     { text: 'Buy meat', done: true, id: '5' },
 // ];
 
+const tasks = [
+    { text: 'Buy milk', done: false, id: '1' },
+    { text: 'Pick up Tom from airport', done: false, id: '2' },
+    { text: 'Visit party', done: false, id: '3' },
+    { text: 'Visit doctor', done: true, id: '4' },
+    { text: 'Buy meat', done: true, id: '5' },
+];
+
+localStorage.setItem('tasksList', JSON.stringify(tasks));
+// let a = localStorage.getItem('struct');
+
 export const setItem = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
 };
