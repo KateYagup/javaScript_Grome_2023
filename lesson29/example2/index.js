8b58db105d09edeb19d9c28e3c7360bae34440f5
kate <kata3140@gmail.com>
2024-05-02 15:24:46 +0300

    example2

--- lesson29/example2/index.js
+++ lesson29/example2/index.js
@@ -1,7 +1,16 @@
-const pinger = (count, period) => {
-    // put your code here
+export const pinger = (count, period) => {
+    let i = count;
+    console.log('Pring');
+    const interval = setInterval(() => {
+        if (--i > 0) {
+            console.log('Ping');
+        } else {
+            clearInterval(interval);
+        }
+    }, period);
+    // console.log(interval);
 };
 
 // examples
 pinger(5, 100); // makes 5 writes with 100 ms interval
-pinger(7, 150); // makes 7 writes with 1500 ms interval
+// pinger(7, 150); // makes 7 writes with 1500 ms interval
