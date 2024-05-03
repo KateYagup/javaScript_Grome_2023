81f45155449cfa9e335a6fc5cadeb0937a94d258
kate <kata3140@gmail.com>
2024-05-03 12:31:13 +0300

    task1

---
+++ lesson29/task1/index.js
@@ -0,0 +1,9 @@
+export const delay = (delay, callback, context, aeg1, arg2, arg3) => {
+    setTimeout(callback, delay);
+}
+
+// const print = () => {
+//     console.log(5);
+// };
+
+// delay(10000, print);
