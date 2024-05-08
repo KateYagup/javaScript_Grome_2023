fcb69df5dd09a4e380058f5b7181b04d2f9e6d4d
kate <kata3140@gmail.com>
2024-05-08 20:26:03 +0300

    new

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,32 +1,14 @@
-export const addImageV2 = url => {
-    const p = new Promise((resolveCb, rejectCb) => {
-        const img = document.createElement('img');
-        img.setAttribute('alt', 'User avatar');
-        img.src = url;
+import { addImage } from './addImage.js';
+const addImageV2 = url => {
 
-        const pageElem = document.querySelector('.page');
-        pageElem.append(img);
-
-        const onImageLoaded = () => {
-            const { width, height } = img;
-            resolveCb({ width, height });
-        };
-
-        // const onImageLoadError = () => callback('Image load failed');
-
-        img.addEventListener('load', onImageLoaded);
-
-        img.addEventListener('error', () => rejectCb(new Error('Image load failed')));
-    });
-    return p;
+    const p = new Promise(resolve => {
+        resolve(addImage);
+        reject(callback);
+    })
 };
 
 // examples
-// const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-// addImageV2(imgSrc)
-//     .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-//     .catch(error => console.log(error)); // ==> 'Image load failed'
 
-// addImageV2('https://server.com/image.png')
-//     .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-//     .catch(error => console.log(error)); // ==> 'Image load failed'
+addImageV2('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg')
+    .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+    .catch(error => console.log(error)); // ==> 'Image load failed'
