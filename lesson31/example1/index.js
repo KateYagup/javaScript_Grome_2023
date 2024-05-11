4712f3437f3172433936236f5041e657cefd36e1
kate <kata3140@gmail.com>
2024-05-11 10:51:17 +0300

    lesson31

---
+++ lesson31/example1/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+// export const requestUserData = userId => {
+//     // put your code here
+// };
+
+export const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500)
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    email: `${userId}`,
+                    userId
+                });
+            }, 1000);
+        }
+
+    });
+
+    return request;
+};
+
+requestUserData('userid777')
+    .catch(error => console.log(error));
+// .then(data => console.log(data));
+
+
+requestUserData('broken-1')
+    .then(data => console.log(data)); 
