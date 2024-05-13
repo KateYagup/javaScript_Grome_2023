0fe672ebe755cc6ba8d58b41b7d5729c262d78bf
kate <kata3140@gmail.com>
2024-05-13 11:58:29 +0300

    lesson32

---
+++ lesson32/example1/index.js
@@ -0,0 +1,21 @@
+const getValueWithDelay = (value, delay) => new Promise(resolve => {
+    setTimeout(() => {
+        console.log(value);
+        resolve(value);
+    }, delay);
+});
+
+const asyncNum1 = getValueWithDelay(56, 2000);
+const asyncNum2 = getValueWithDelay(4, 1000);
+const asyncNum3 = getValueWithDelay(10, 500);
+
+const getSum = numbers =>
+    numbers.reduce((acc, num) => acc + num, 0);
+
+const asyncSum = (...asyncNumbers) => {
+    return Promise.all(asyncNumbers)
+        .then(numbers => getSum(numbers));
+};
+
+asyncSum(asyncNum1, asyncNum2, asyncNum3)
+    .then(result => console.log(result));
