0d0983a732639beb40c2c11c6ad72a9e30be91a0
kate <kata3140@gmail.com>
2024-05-30 16:16:04 +0300

    new

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -2,18 +2,25 @@ const baseUrl = `https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks`;
 // const baseUrl1 = `api.github.com/repos/USERID/REPOID/commits?per_page=100`;
 
 function getTasksList() {
-    return fetch(baseUrl)
-        .then(response => response.json());
+    console.log('Работа функции');
+    const result = fetch(baseUrl)
+        .then(response => response.json())
+        .then(result => console.log(result.map(result => console.log(result.id))));
+    return result;
 }
 
 function getTaskById(taskId) {
-    // put your code here
+    return fetch(baseUrl)
+        .then(response => response.json())
+        .then(response => console.log(response.id));
 }
 
 // examples
+console.log('Программа');
 getTasksList().then(tasksList => {
     console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
 });
+// getTasksList();
 
 // getTaskById('2').then(taskData => {
 //     console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
