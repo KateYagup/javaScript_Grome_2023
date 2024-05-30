5844600b60c23fc73eee5bcba959e1160ee1a90d
kate <kata3140@gmail.com>
2024-05-30 16:36:24 +0300

    new

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -1,28 +1,23 @@
 const baseUrl = `https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks`;
-// const baseUrl1 = `api.github.com/repos/USERID/REPOID/commits?per_page=100`;
 
-function getTasksList() {
-    console.log('Работа функции');
-    const result = fetch(baseUrl)
+export function getTasksList() {
+    return result = fetch(baseUrl)
         .then(response => response.json())
-        .then(result => console.log(result.map(result => console.log(result.id))));
-    return result;
+    // .then(result => console.log(result.map(result => console.log(result.id))));
 }
 
-function getTaskById(taskId) {
+export function getTaskById(taskId) {
     return fetch(baseUrl)
         .then(response => response.json())
         .then(response => response.filter(response => { if (response.id === taskId) return true }))
-        .then(result => console.log(result));
+    // .then(result => console.log(result));
 }
 
 // examples
-console.log('Программа');
-getTasksList().then(tasksList => {
-    console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
-});
-// getTasksList();
+// getTasksList().then(tasksList => {
+//     console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+// });
 
-getTaskById('2').then(taskData => {
-    console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
-});
+// getTaskById('2').then(taskData => {
+//     console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+// });
