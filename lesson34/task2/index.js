67e6bc6cfc963e273e95117ed3badc39c5712e03
kate <kata3140@gmail.com>
2024-06-06 16:09:54 +0300

    task2

---
+++ lesson34/task2/index.js
@@ -0,0 +1,54 @@
+// const { check } = require("prettier");
+
+const baseUrl = 'http://665d7308e88051d604069f45.mockapi.io/api/v1/users';
+
+const formElem = document.querySelector('.login-form');
+const buttonElem = document.querySelector('.submit-button');
+
+const checkValid = () => {
+    const determineValid = formElem.reportValidity();
+    console.log(determineValid);
+    if (determineValid) {
+        buttonElem.removeAttribute('disabled');
+    }
+    console.log(buttonElem);
+};
+
+formElem.addEventListener('input', checkValid);
+
+const saveData = () => {
+    const newData = Object.entries(new FormData(formElem));
+    console.log(newData);
+}
+// buttonElem.addEventListener('click', saveData);
+const onFormSubmit = event => {
+    event.preventDefault();
+    console.log('onFormSubmit');
+    const newData = Object.values(...new FormData(formElem));
+    console.log(newData);
+    const nameValue = new FormData(formElem).get('name');
+    const passwordValue = new FormData(formElem).get('password');
+    const emailValue = new FormData(formElem).get('email');
+
+    const newUserData = {
+        nameValue,
+        passwordValue,
+        emailValue,
+    }
+
+    fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(newUserData)
+    }).then(() => console.log('User created'));
+
+    fetch(baseUrl)
+        .then(result => result.json())
+        .then(users => console.log(users));
+
+
+}
+formElem.addEventListener('submit', onFormSubmit)
+
