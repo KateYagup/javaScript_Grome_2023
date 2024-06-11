fa8e8e56dc28159a68618a843df5ae54e63fdff8
kate <kata3140@gmail.com>
2024-06-11 18:05:20 +0300

    new

--- lesson35/task1/index.js
+++ lesson35/task1/index.js
@@ -3,13 +3,17 @@ export const parseUser = jsonString => {
     try {
         // const jsonString = '{"name":"Tom"';
         const userData = JSON.parse(jsonString);
-        console.log(userData);
+        return userData;
+        // console.log(userData);
     } catch (err) {
         if (err instanceof SyntaxError) {
-            console.error(err.name);
+            return null;
+            // console.error(err.name);
         } else {
-            throw err;
+            return null;
         }
-    }
+    };
 };
+
+// https://avatars3.githubusercontent.com/u10001
 // parseUser();
