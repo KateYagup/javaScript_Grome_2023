072f4984a833b08893c5b33285ea39432dff8c99
kate <kata3140@gmail.com>
2024-06-20 17:21:37 +0300

    new

--- lesson36/task1/index.js
+++ lesson36/task1/index.js
@@ -1,10 +1,10 @@
-// /**
-//  * @param {string[]} users
-//  * @return {promise}
-//  */
+/**
+ * @param {string[]} users
+ * @return {promise}
+ */
 
 
-const getUsersBlogs = async users => {
+export const getUsersBlogs = async users => {
     const usersUrl = await users.map(user => {
         const result = fetch(`https://api.github.com/users/${user}`)
             .then(response => response.json())
@@ -21,5 +21,5 @@ const getUsersBlogs = async users => {
 };
 
 // examples
-getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
